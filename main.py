0c64d3546e18095f164f35ef49a91964c65d1fcd
anita20 <a.otoo@alustudent.com>
2026-09-13 21:26:53 +0000

    adding menu option

--- main.py
+++ main.py
@@ -1,7 +1,67 @@
-print("========================================")
-print("Clinic Appointment And Management System")
-print("========================================")
 
+def main():# this particular function will control the main flow
+              #of the application'
+
+    while True:
+        #display the opening screen
+
+        print("========================================")
+        print("Clinic Appointment And Management System")
+        print("========================================")
+        print()
+        print("Welcome!")
+        print()
+        print("Enter your ID to continue.")
+        print("Type NEW to register as patient. ")
+        print("Type EXIT to close the system")
+        print()
+
+
+
+    #Ask the user for their ID  
+        user_id =input("Enter your ID: ").strip()
+
+
+        if user_id.lower() =="exit":
+            #close the system when the user chooses Exit
+            print("Thank you for using the Clinic Management System.")
+            print("System closed successfully")
+            break
+
+        elif user_id.lower() =="new": 
+             #start the registration process for a new patient
+            register_patient()
+
+        elif user_id.startswith("A-"):
+             # An ID starting with A belongs to the Adminstrator
+            print("Opening Adminstrator Menu...")
+            admin_menu() 
+            
+
+        elif user_id.startswith("DR-"):
+            print("Opening Doctor Menu... ")
+            #open the doctor's menu
+            doctor_menu()
+
+        elif user_id.startswith("P-"):
+            #An ID starting with P- belongs to patient
+            print("Opening Patient Menu ...")
+
+            patient_menu()
+            #an ID starting with P belongs to a patient.
+            
+
+
+        else:
+            #this runs when the ID does not match
+            #any of the accepted ID formats
+            print("Invalid ID.Please enter a valid ID.")
+
+
+    
+#This starts the program by calling the main function
+        
+            
 
 
 def admin_menu():
@@ -9,13 +69,11 @@ def admin_menu():
     #to an adminstrator.
 
     while True:
-<<<<<<< HEAD
-        print("=====ADMINISTRATOR MENU=====")
-=======
-        print("====ADMINISTRATOR MENU====")
->>>>>>> 981d3a7 (connecting patient menu)
+        print("=================")
+        print("Adminstrator Menu")
+        print("=================")
         print("1.Register new patient")
-        print("2.Display all patients")
+        print("2.Display all  patients")
         print("3.Search patient")
         print("4.Update patient information")
         print("5.Add doctor")
@@ -27,7 +85,23 @@ def admin_menu():
         if  choice =="7":
             print("Logging out...")
             break
+        elif choice =="1":
+            print("Pegister new patient")
 
+        elif choice =="2":
+            print("Display all patient")
+
+        elif choice =="3":
+            print("Search patients")
+
+        elif choice =="4":
+            print("Update patient information")
+
+        elif choice =="5":
+            print("Add doctor")
+
+        elif choice =="6":
+            print("View appointments")
 
         else:
             print("Option selected:",choice)
@@ -39,88 +113,87 @@ def  doctor_menu():
 
 
     while True:
-        print("=====DOCTOR MENU=====")
+        print("===========")
+        print("Doctor Menu")
+        print("===========")
         print("1.View appointment")
         print("2.View patient information")
         print("3.Update appointment")
         print("4.Logout")
 
-
         choice = input("Choose an option: ")
 
-
         if choice =="4":
-            print("Logging out...")
+            print("View appointment")
+            break
 
+        elif choice =="1":
+            print("View appointment")
+
+        elif choice =="2":
+                    print("View patient information")
+
+        elif choice =="3":
+            print("Update appointment")
 
         else:
-            print("Option selected:", choice)
+            print("Invalid option.Please choose a number from 1 to 4")
 
+
+        
 
 
 def patient_menu():
 
 
     while True:
-        print("========PATIENT MENU========")
+        print("============")
+        print("Patient Menu")
+        print("============")
         print("1.View my infomation")
         print("2.Book appointment")
         print("3.View my appointment")
         print("4.Cancel appointment")
         print("5.Logout")
 
-        choice = input("Choose an option ")
+        choice = input("Choose an option: ")
 
         if choice =="5":
             print("Logging out...")
-            break
+            break 
+
+        elif choice =="1":
+            print("View my information")
+
+        elif choice =="2":
+            print("Book appointment")
+
+        elif choice =="3":
+            print("View my appointment")
+
+        elif choice =="4":
+            print("Cancel appointment")
 
 
         else:
-            print("Option selected:", choice)
+            print("Invalid optiom.Please choose a number from 1 to 5.")
+        
+def register_patient():
+    #this function will collect the information
+    #needed to register a new patient
 
-def main():  # this particular function will control the main flow
-   
-
-    while True:
-        #Keep the system running until the user chooses to exit
-        print("Clinic system is running ...")  
-
-    #Ask the user for their ID or type Exit 
-        user_id =input("Enter your ID ,type NEW for registration,or Exit to close: ")
-
-        if user_id =="Exit":
-            #close the system when the user chooses Exit
-            print("Thank you for using the Clinic Management System.")
-            print("System closed successfully")
-            break
-        elif user_id =="NEW":  #start the registration process for a new patient
-            print("Starting new patient registration")
-
-        elif user_id.startswith("A-"): # An ID starting with A belongs to the Adminstrator
-
-            admin_menu() 
-
-            print("Opening Adminstrator Menu")
-
-        elif user_id.startswith("DR-"):
-            print("Opening Doctor Menu... ")
-            #open the doctor's menu
-            doctor_menu()
-
-        elif user_id.startswith("P"):
-
-            patient_menu()
-            #an ID starting with P belongs to a doctor.
-            print("Opening Patient Menu")
+    print("=====================")
+    print("Patient Registration ")
+    print("=====================")
 
 
-        else:
-            #this runs when the ID does not match
-            #any of the accepted ID formats
-            print("Invalid ID.Please enter a valid ID.")
+    name = input("Enter your name: ")
+    phone_number = input("Enter your phone number: ")
+    gender = input("Enter your gender: ")
+    date_of_birth = input("Enter your date of birth: ")
+    email = input("Enter your email: ")
+    address = input("Enter your address: ")
 
-
-    print("Welcome to the System!")
-
-main()
+    print()
+    print("Patient information collected successfully.")
+main()       
